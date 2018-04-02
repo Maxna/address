3bf39a0843b884a8eab42b706d9d32f56e762c1c
Garnett Puett and Jared Lutz <garnett@>
2018-04-02 11:40:58 -0700

    Add more jquery

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,21 @@
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
+  this.addresses = [];
+}
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
 }
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
+}
+
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state;
 }
 
 // user interface logic
@@ -18,6 +29,14 @@ $(function() {
 
     var newContact = new Contact(inputtedFirstName, inputtedlastName);
 
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find(".new-street").val();
+      var inputtedCity = $(this).find(".new-city").val();
+      var inputtedState = $(this).find(".new-state").val();
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
+      newContact.addresses.push(newAddress);
+    });
+
     $("#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
     $(".contact").last().click(function() {
@@ -25,6 +44,10 @@ $(function() {
       $("#show-contact h2").text(newContact.firstName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
+      $("#addresses").text("");
+      newContact.addresses.forEach(function(address) {
+        $("#addresses").append("<li>" + address.fullAddress() + "</li>");
+      });
     });
 
     $("#new-first-name").val("");
